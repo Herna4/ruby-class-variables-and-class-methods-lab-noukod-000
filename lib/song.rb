45bf6f3ad1f4abd6c1d9a843865bab05162ac515
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-16 19:52:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,3 +9,15 @@ class Song
   
 end
 
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ 
+ninety_nine_problems.name
+ 
+ 
+ninety_nine_problems.artist
+
+ 
+ninety_nine_problems.genre
+  # => "rap"
+
