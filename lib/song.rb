d87f95f36869b7801c45a8895e7dfb6a992372ce
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-16 20:26:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,13 +27,13 @@ class Song
   end
 
    def self.genre_count
-    genre_count = Hash.new(0)
+    genre_count = Hash.new
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
   end
 
    def self.artist_count
-    artist_count = Hash.new(0)
+    artist_count = Hash.new
     @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
